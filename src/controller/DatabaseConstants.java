bcd5f717090efd7b2afa06545ac538e37a783074
auspenskiy <uspenskyandrey@gmail.com>
2015-05-25 14:57:20 -0400

    added database constants class

---
+++ src/controller/DatabaseConstants.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class DatabaseConstants
+{
+	public static final String PROJECT_MANAGEMENT_DB = "jdbc:sqlite:projectManagement.db";
+	//public static final String TEST_DB = "jdbc:sqlite:projectManagement.db";
+}
