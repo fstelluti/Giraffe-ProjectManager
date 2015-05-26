5aeb43adf1d05306887fa85040d2266c25732801
Lukas <lukascgoyette@hotmail.com>
2015-05-26 17:00:52 -0400

    Comments and some GUI in the edit project dialog class

--- src/view/EditProjectDialog.java
+++ src/view/EditProjectDialog.java
@@ -9,20 +9,32 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import model.Project;
+import controller.DataManager;
+import controller.DatabaseConstants;
+
 public class EditProjectDialog extends JDialog 
 {
-
-	private JTextField projectName, projectManager;
-	private JLabel projectNameLabel, projectManagerLabel;
+	 private JComboBox projectBox;
+	 private JTextField projectName, projectManager;
+	 private JLabel projectNameLabel, projectManagerLabel, dueDateLabel, startDateLabel, projectLabel;
+	 private JFormattedTextField dueDate, startDate;
+	 private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
   public EditProjectDialog(JFrame parent, String title, boolean modal)
   {
@@ -37,16 +49,21 @@ public class EditProjectDialog extends JDialog
   
   private void initComponent()
   {
-	  //Project Name - Maybe change to drop-down list containing all current projects
-	  JPanel panName = new JPanel();
-	  panName.setBackground(Color.white);
-	  panName.setPreferredSize(new Dimension(220, 60));
-	  projectName = new JTextField();
-	  projectName.setPreferredSize(new Dimension(100, 25));
-	  panName.setBorder(BorderFactory.createTitledBorder("Project Name"));
-	  projectNameLabel = new JLabel("Project Name :");
-	  panName.add(projectNameLabel);
-	  panName.add(projectName);
+	  JPanel panProjectName = new JPanel();
+	  panProjectName.setBackground(Color.white);
+	  panProjectName.setPreferredSize(new Dimension(465, 60));
+	  
+	  //CODE HERE WILL PULL AN ARRAY OF PROJECT NAMES
+	  final List<Project> projects = DataManager.getProjects(DatabaseConstants.PROJECT_MANAGEMENT_DB);
+	  String[] projectNames = new String[projects.size()];
+	  for(int i = 0; i < projectNames.length; i++){
+		  projectNames[i] = projects.get(i).getProjectName();
+	  }
+	  projectBox = new JComboBox(projectNames);
+	  panProjectName.setBorder(BorderFactory.createTitledBorder("Project"));
+	  projectLabel = new JLabel("Select Project:");
+	  panProjectName.add(projectLabel);
+	  panProjectName.add(projectBox);
 	  
 	  //Project Manager
 	  JPanel panManager = new JPanel();
@@ -55,11 +72,31 @@ public class EditProjectDialog extends JDialog
 	  projectManager = new JTextField();
 	  projectManager.setPreferredSize(new Dimension(100, 25));
 	  panManager.setBorder(BorderFactory.createTitledBorder("PM Name"));
-	  projectManagerLabel = new JLabel("Project Manager Name :");
+	  projectManagerLabel = new JLabel("PM Name :");
 	  panManager.add(projectManagerLabel);
 	  panManager.add(projectManager);
 	  
+	  //Start Date
+	  JPanel panStartDate = new JPanel();
+	  panStartDate.setBackground(Color.white);
+	  panStartDate.setPreferredSize(new Dimension(220, 60));
+	  startDate = new JFormattedTextField(dateFormat.format(new Date()));
+	  startDate.setPreferredSize(new Dimension(100, 25));
+	  panStartDate.setBorder(BorderFactory.createTitledBorder("Start Date (YYYY-MM-DD)"));
+	  startDateLabel = new JLabel("Start Date:");
+	  panStartDate.add(startDateLabel);
+	  panStartDate.add(startDate);
+	  
 	  //Due date
+	  JPanel panDueDate = new JPanel();
+	  panDueDate.setBackground(Color.white);
+	  panDueDate.setPreferredSize(new Dimension(220, 60));
+	  dueDate = new JFormattedTextField(dateFormat.format(new Date()));
+	  dueDate.setPreferredSize(new Dimension(100, 25));
+	  panDueDate.setBorder(BorderFactory.createTitledBorder("Due Date (YYYY-MM-DD)"));
+	  dueDateLabel = new JLabel("Due Date:");
+	  panDueDate.add(dueDateLabel);
+	  panDueDate.add(dueDate);
 	  
 	  
 	  JPanel control = new JPanel();
@@ -80,8 +117,10 @@ public class EditProjectDialog extends JDialog
 	  
 	  JPanel content = new JPanel();
 	  content.setBackground(Color.white);
-	  content.add(panName);
+	  content.add(panProjectName);
 	  content.add(panManager);
+	  content.add(panDueDate);
+	  content.add(panStartDate);
 	  
 	  this.getContentPane().add(content, BorderLayout.CENTER);
 	  this.getContentPane().add(control, BorderLayout.SOUTH);
