a0c430fa38e2413e86087dab2ca77f5aac9d3cd5
swagrvd <francoisstelluti@gmail.com>
2015-05-31 23:12:16 -0400

    Updated Testing for Users and ViewManager

--- test/controller/ViewManagerTest.java
+++ test/controller/ViewManagerTest.java
@@ -15,18 +15,17 @@ public class ViewManagerTest {
 	
 	@Test
 	public void shouldCreateMainViewPanel() {
+	  //Create a User with test variables 
+	  User user1 = new User(1,"user1","pass1","test@aol.com","Dave", "Johnson");
 		
-		//Create a User with test variables 
-		User user1 = new User(1,"user1","pass1","test@aol.com","Dave", "Johnson");
+	  //Need to run the startApplication method before testing if the main view panel was created
+	  //This avoids a Null pointer exception
+	  ViewManager.startApplication();
 		
-		//Need to run the startApplication method before testing if the main view panel was created
-		//This avoids a Null pointer exception
-		ViewManager.startApplication();
+	  //Create a JPanel and simply test if it is not null at the end
+      JPanel mainViewPanel = ViewManager.createMainViewPanel(user1);
 		
-		//Create a JPanel and simply test if it is not null at the end
-		JPanel mainViewPanel = ViewManager.createMainViewPanel(user1);
-		
-		assertNotNull("The Main view panel was not created!", mainViewPanel);
+	  assertNotNull("The Main view panel was not created!", mainViewPanel);
 	}
 	
 // This method will have to be tested through the GUI
@@ -35,20 +34,20 @@ public class ViewManagerTest {
 
 	@Test
 	public void shouldStartApplication() {
+	  ApplicationPanel applicationPanel = new ApplicationPanel();
 		
-		ApplicationPanel applicationPanel = new ApplicationPanel();
-		
-		//Test that the Application Panel object was created
-		assertNotNull("The Application Panel was not created!", applicationPanel);
+	  //Test that the Application Panel object was created
+	  assertNotNull("The Application Panel was not created!", applicationPanel);
 	}
 
-	@Test
-	public void shouldCreateAccountDialog() {
-		
-		JDialog dialog = new JDialog();
-		
-		//Test that the Dialog object was created
-		assertNotNull("The Create account dialog was not created!", dialog);
-	}
+//  Might not be needed, method currentely returns void
+//	@Test
+//	public void shouldCreateAccountDialog() {
+//		
+//		JDialog dialog = new JDialog();
+//		
+//		//Test that the Dialog object was created
+//		assertNotNull("The Create account dialog was not created!", dialog);
+//	}
 
 }
--- test/model/UserTest.java
+++ test/model/UserTest.java
@@ -1,179 +1,190 @@
 package model;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import java.util.Date;
 
 //This class will test the methods in the model/User.java Class
 public class UserTest {
 
 	@Test
 	public void userShouldBeCreated() {
+	  //Test variables for the constructor
+	  int id = 5;
+	  String userName = "Test1";
+	  String password = "1234A";
+	  String email = "test@test.com";
+	  String firstName = "Frank";
+	  String lastName = "Stone";
+	  Date regDate = new Date();
 		
-		//Test variables for the constructor
-		int id = 5;
-		String userName = "Test1";
-		String password = "1234A";
-		String email = "test@test.com";
-		String firstName = "Frank";
-		String lastName = "Stone";
+	  User testUser = new User(5, userName, password, email, firstName, lastName);
 		
-		User testUser = new User(5, userName, password, email, firstName, lastName);
+	  //Test that the User object was created
+	  assertNotNull("The user was not created!", testUser);
 		
-		//Test that the User object was created
-		assertNotNull("The user was not created!", testUser);
-		
-		//Test that parameters were passed into the Object
-		assertEquals("ID didn't match", testUser.getId(), id);
-		assertEquals("Username didn't match", testUser.getUserName(), userName);
-		assertEquals("Password didn't match", testUser.getPassword(), password);
-		assertEquals("Email didn't match", testUser.getEmail(), email);
-		assertEquals("First name didn't match", testUser.getFirstName(), firstName);
-		assertEquals("Last name didn't match", testUser.getLastName(), lastName);
+	  //Test that parameters were passed into the Object
+      assertEquals("ID didn't match", testUser.getId(), id);
+	  assertEquals("Username didn't match", testUser.getUserName(), userName);
+	  assertEquals("Password didn't match", testUser.getPassword(), password);
+	  assertEquals("Email didn't match", testUser.getEmail(), email);
+	  assertEquals("First name didn't match", testUser.getFirstName(), firstName);
+	  assertEquals("Last name didn't match", testUser.getLastName(), lastName);
+	  assertEquals("No date was created", testUser.getRegDate(), regDate);
 	}
 
 	@Test
 	public void shouldGetUserName() {
+	  //Only create User object with a user name
+	  String userName = "testy1";
 		
-		//Only create User object with a user name
-		String userName = "testy1";
+	  User testUser = new User(0, userName, null, null, null, null);
 		
-		User testUser = new User(0, userName, null, null, null, null);
-		
-		assertEquals("Did not get correct user name", testUser.getUserName(), userName);
+	  assertEquals("Did not get correct user name", testUser.getUserName(), userName);
 	}
 
 	@Test
 	public void shouldSetUserName() {
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
 		
-		//Create an "empty" Object 
-		User testUser = new User(0, null, null, null, null, null);
+	  //Variable used to set an attribute of the object
+	  String userName = "testy1";
+	  testUser.setUserName(userName);
 		
-		//Variable used to set an attribute of the object
-		String userName = "testy1";
-		testUser.setUserName(userName);
-		
-		assertNotNull("Username not set properly", testUser.getUserName());
+	  assertNotNull("Username not set properly", testUser.getUserName());
 	}
 
 	@Test
 	public void shouldGetPassword() {
+	  //Only create User object with a password
+	  String password = "3456&*";
 		
-		//Only create User object with a password
-		String password = "3456&*";
+	  User testUser = new User(0, null, password, null, null, null);
 		
-		User testUser = new User(0, null, password, null, null, null);
-		
-		assertEquals("Did not get correct password", testUser.getPassword(), password);
+	  assertEquals("Did not get correct password", testUser.getPassword(), password);
 	}
 
 	@Test
 	public void shouldSetPassword() {
-
-		//Create an "empty" Object 
-		User testUser = new User(0, null, null, null, null, null);
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
 		
-		//Variable used to set an attribute of the object
-		String password = "0987ko*";
-		testUser.setPassword(password);
+	  //Variable used to set an attribute of the object
+	  String password = "0987ko*";
+	  testUser.setPassword(password);
 		
-		assertNotNull("Password not set properly", testUser.getPassword());
+	  assertNotNull("Password not set properly", testUser.getPassword());
 	}
 
 	@Test
 	public void shouldGetEmail() {
+	  //Only create User object with an email address
+	  String email = "test@lol.com";
 		
-		//Only create User object with an email address
-		String email = "test@lol.com";
+	  User testUser = new User(0, null, null, email, null, null);
 		
-		User testUser = new User(0, null, null, email, null, null);
-		
-		assertEquals("Did not get correct email", testUser.getEmail(), email);
+	  assertEquals("Did not get correct email", testUser.getEmail(), email);
 	}
 
 	@Test
 	public void shouldSetEmail() {
-
-		//Create an "empty" Object 
-		User testUser = new User(0, null, null, null, null, null);
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
 		
-		//Variable used to set an attribute of the object
-		String email = "test@aol.com";
-		testUser.setEmail(email);
+	  //Variable used to set an attribute of the object
+	  String email = "test@aol.com";
+	  testUser.setEmail(email);
 		
-		assertNotNull("Email not set properly", testUser.getEmail());
+	  assertNotNull("Email not set properly", testUser.getEmail());
 	}
 
 	@Test
 	public void shouldGetFirstName() {
-
-		//Only create User object with a first name
-		String firstName = "John";
+	  //Only create User object with a first name
+	  String firstName = "John";
 		
-		User testUser = new User(0, null, null, null, firstName, null);
+	  User testUser = new User(0, null, null, null, firstName, null);
 		
-		assertEquals("Did not get correct first name", testUser.getFirstName(), firstName);
+	  assertEquals("Did not get correct first name", testUser.getFirstName(), firstName);
 	}
 
 	@Test
 	public void shouldSetFirstName() {
-
-		//Create an "empty" Object 
-		User testUser = new User(0, null, null, null, null, null);
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
+	
+	  //Variable used to set an attribute of the object
+	  String firstName = "Jenny";
+	  testUser.setFirstName(firstName);
 		
-		//Variable used to set an attribute of the object
-		String firstName = "Jenny";
-		testUser.setFirstName(firstName);
-		
-		assertNotNull("First name not set properly", testUser.getFirstName());
+	  assertNotNull("First name not set properly", testUser.getFirstName());
 	}
 
 	@Test
 	public void shouldGetLastName() {
-
-		//Only create User object with a last name
-		String lastName = "Green";
+	  //Only create User object with a last name
+	  String lastName = "Green";
 		
-		User testUser = new User(0, null, null, null, null, lastName);
+	  User testUser = new User(0, null, null, null, null, lastName);
 		
-		assertEquals("Did not get correct last name", testUser.getLastName(), lastName);
+	  assertEquals("Did not get correct last name", testUser.getLastName(), lastName);
 	}
 
 	@Test
 	public void shouldSetLastName() {
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
 		
-		//Create an "empty" Object 
-		User testUser = new User(0, null, null, null, null, null);
+	  //Variable used to set an attribute of the object
+	  String lastName = "Darko";
+	  testUser.setLastName(lastName);
 		
-		//Variable used to set an attribute of the object
-		String lastName = "Darko";
-		testUser.setLastName(lastName);
-		
-		assertNotNull("Last name not set properly", testUser.getLastName());
+	  assertNotNull("Last name not set properly", testUser.getLastName());
 	}
 
 	@Test
 	public void shouldGetId() {
-
-		//Only create User object with a user ID
-		int ID = 7;
+	  //Only create User object with a user ID
+	  int ID = 7;
 		
-		User testUser = new User(7, null, null, null, null, null);
+	  User testUser = new User(7, null, null, null, null, null);
 		
-		assertEquals("Did not get correct user ID", testUser.getId(), ID);
+	  assertEquals("Did not get correct user ID", testUser.getId(), ID);
 	}
 
 	@Test
 	public void shouldSetId() {
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
 		
-		//Create an "empty" Object 
-		User testUser = new User(0, null, null, null, null, null);
+	  //Variable used to set an attribute of the object
+	  int ID = 18;
+	  testUser.setId(ID);
 		
-		//Variable used to set an attribute of the object
-		int ID = 18;
-		testUser.setId(ID);
+	  assertEquals("ID not set properly", testUser.getId(), ID);
+	}
+	
+	@Test
+	public void shouldGetRegDate() {
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
 		
-		assertEquals("ID not set properly", testUser.getId(), ID);
+	  assertNotNull("Registration date does not exist", testUser.getRegDate());
+	}
+	
+	@Test
+	public void shouldSetRegDate() {
+	  //Create an "empty" Object 
+	  User testUser = new User(0, null, null, null, null, null);
+		
+	  //Variable used to set an attribute of the object
+	  Date regDate = new Date();
+	  //Convert Date to a String
+	  String stringDate = regDate.toString();
+	  testUser.setRegDate(regDate);
+		
+	  assertEquals("RegDate not set properly", testUser.getRegDate().toString(), stringDate);
 	}
 
 }
