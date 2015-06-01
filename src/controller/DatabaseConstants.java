5ae4e78e017d46f3b4c4dd611f3d62da1d86d944
Zachary Bergeron <zbergeron01@gmail.com>
2015-05-31 21:28:39 -0400

    modified id manually

--- src/controller/DatabaseConstants.java
+++ src/controller/DatabaseConstants.java
@@ -2,6 +2,7 @@ package controller;
 
 public class DatabaseConstants
 {
-	public static String PROJECT_MANAGEMENT_DB = "jdbc:sqlite:projectManagement.db";
+//	public static String PROJECT_MANAGEMENT_DB = "jdbc:sqlite:projectManagement.db";
+	public static String PROJECT_MANAGEMENT_DB = "jdbc:sqlite:myProjectManagement.db";
 	//public static final String TEST_DB = "jdbc:sqlite:projectManagement.db";
 }
