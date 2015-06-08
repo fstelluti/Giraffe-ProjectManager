5d7ccb376023793f644a76118f3d26bb69ccdc6a
Zachary Bergeron <zbergeron01@gmail.com>
2015-06-08 16:05:46 -0400

    Right hand summary (Temp)

--- src/view/GridProjects.java
+++ src/view/GridProjects.java
@@ -30,39 +30,39 @@ public class GridProjects extends JPanel
 		
 		//UNCOMMENT THE FOLLOWING TO ACTIVATE THE INITIAL GRID VIEW (VERY EARLY STAGES)
 		
-//		try {
-//			grid = new JTable(buildTableModel(ProjectDB.RSgetUserProjects(connectionString, user.getId())) );
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			grid = new JTable(buildTableModel(ProjectDB.RSgetUserProjects(connectionString, user.getId())) );
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
-//		this.add(grid, BorderLayout.CENTER);
+		this.add(grid, BorderLayout.NORTH);
 		
 	}
 	
-//	public static DefaultTableModel buildTableModel(ResultSet rs)
-//	        throws SQLException {
-//
-//	    ResultSetMetaData metaData = rs.getMetaData();
-//
-//	    // names of columns
-//	    Vector<String> columnNames = new Vector<String>();
-//	    int columnCount = metaData.getColumnCount();
-//	    for (int column = 1; column <= columnCount; column++) {
-//	        columnNames.add(metaData.getColumnName(column));
-//	    }
-//
-//	    // data of the table
-//	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-//	    while (rs.next()) {
-//	        Vector<Object> vector = new Vector<Object>();
-//	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-//	            vector.add(rs.getObject(columnIndex));
-//	        }
-//	        data.add(vector);
-//	    }
-//
-//	    return new DefaultTableModel(data, columnNames);
-//
-//	}
+	public static DefaultTableModel buildTableModel(ResultSet rs)
+	        throws SQLException {
+
+	    ResultSetMetaData metaData = rs.getMetaData();
+
+	    // names of columns
+	    Vector<String> columnNames = new Vector<String>();
+	    int columnCount = metaData.getColumnCount();
+	    for (int column = 1; column <= columnCount; column++) {
+	        columnNames.add(metaData.getColumnName(column));
+	    }
+
+	    // data of the table
+	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    while (rs.next()) {
+	        Vector<Object> vector = new Vector<Object>();
+	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+	            vector.add(rs.getObject(columnIndex));
+	        }
+	        data.add(vector);
+	    }
+
+	    return new DefaultTableModel(data, columnNames);
+
+	}
 }
