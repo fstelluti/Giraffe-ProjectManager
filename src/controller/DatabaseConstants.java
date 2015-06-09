59f1291422c0f3671609e10c515427a3cd40635f
auspenskiy <uspenskyandrey@gmail.com>
2015-06-08 20:04:14 -0400

    Revert "fixes"
    This reverts commit cce8b83731b89de8d0ecd13655d1be60ed2e1487.

--- src/controller/DatabaseConstants.java
+++ src/controller/DatabaseConstants.java
@@ -4,5 +4,5 @@ public class DatabaseConstants
 {
 	//public static String PROJECT_MANAGEMENT_DB = "jdbc:sqlite:projectManagement.db";
 	public static String PROJECT_MANAGEMENT_DB = "jdbc:sqlite:myProjectManagement.db";
-
+	//public static final String TEST_DB = "jdbc:sqlite:projectManagement.db";
 }
