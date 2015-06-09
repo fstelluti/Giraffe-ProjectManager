8f05e932486159c20e1a8a92fe99fa57371f9e8d
Francois <francoisstelluti@gmail.com>
2015-06-09 10:59:54 -0400

    Small fix in ActivityTest

--- test/model/ActivityTest.java
+++ test/model/ActivityTest.java
@@ -15,9 +15,7 @@ public class ActivityTest {
 
 	//Make a status variable, set to default of 1, and a status array
 	private int status = 1;
-	@SuppressWarnings("unused")
-	private String[] statusArray = new String[]{"To do", "In Progress", "Completed"};
-	
+
 	//Tests setStatus()
 	@Test
 	public void testSetStatus() {
