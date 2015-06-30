05fd59a9377b6a43b360a7357d87c701171fd525
Zachary Bergeron <zbergeron01@gmail.com>
2015-06-30 11:29:28 -0400

    Commented out non used test classes

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -4,12 +4,13 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	controller.ActivityDBTest.class,
+	controller.DatabaseConstantsTest.class,
 	controller.DataManagerTest.class,
 	controller.PredecessorDBTest.class,
 	controller.ProjectDBTest.class,
 	controller.UserDBTest.class,
 	controller.UserRolesDBTest.class,
-	controller.UserRolesDictDBTest.class,
+//	controller.UserRolesDictDBTest.class,
 	controller.ViewManagerTest.class,
 	model.UserTest.class,
 	model.ActivityTest.class,
