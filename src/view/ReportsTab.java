611d5bab033e9c42151dea0073a09791f7aa850a
auspenskiy <uspenskyandrey@gmail.com>
2015-08-09 12:49:15 -0400

    pert report (not finished yet)

--- src/view/ReportsTab.java
+++ src/view/ReportsTab.java
@@ -11,17 +11,20 @@ public class ReportsTab extends JPanel {
     private static final long serialVersionUID = 6105597890457941706L;
     private GanttPanel ganttPanel;
     private EarnedValueAnalysisTab earnedValueAnalysisTab;
+    private PertAnalysisTab pertAnalysisTab;
     
     public ReportsTab () {
 			JTabbedPane tabPane = new JTabbedPane();
 			this.ganttPanel = new GanttPanel();
 			this.earnedValueAnalysisTab = new EarnedValueAnalysisTab();
+			this.pertAnalysisTab = new PertAnalysisTab();
 			
 			//Icons
 			ImageIcon evaIcon = new ImageIcon(TabPanel.class.getResource("images/chart_stock.png"));
 			
 			tabPane.addTab("Gantt Chart", null, this.ganttPanel, "View Gantt chart for the currently selected project");
 			tabPane.addTab("Earned Value Analysis", evaIcon, earnedValueAnalysisTab, "Generate Earned Value Analysis for the currently selected project");
+			tabPane.addTab("PERT Analysis", evaIcon, pertAnalysisTab, "Generate PERT Analysis for the currently selected project");
 			this.setLayout(new BorderLayout());
 			this.add(tabPane);
     }
@@ -29,5 +32,6 @@ public class ReportsTab extends JPanel {
     public void refresh() {
     	ganttPanel.refresh();
     	earnedValueAnalysisTab.refresh();
+    	pertAnalysisTab.refresh();
     }
 }
